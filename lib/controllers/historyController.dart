c7f872877cd17e41018fff8fab459032ca35ed8b
Santiago Guerrero Martinez <47578270+CodiPop@users.noreply.github.com>
2023-03-11 20:59:30 -0500

    Boxex by number of items implemented

---
+++ lib/controllers/historyController.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class historyController extends GetxController {
+  final List<String> items =
+      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"].obs;
+}
--- lib/ui/pages/history-screen.dart
+++ lib/ui/pages/history-screen.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../controllers/historyController.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
-
+  static historyController myController = Get.put(historyController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HistoryScreen'),
+        title: const Text('Historial'),
       ),
       body: Center(
-        child: Text("HistoryScreen"),
+        child: ListView.builder(
+          itemCount: myController.items.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                width: MediaQuery.of(context).size.width * 0.8,
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.all(8),
+                child: Text(
+                  myController.items[index],
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
