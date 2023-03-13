d22fe35d16acfb3219faa45d7d36d62ce4dcb0f4
Santiago Guerrero Martinez <47578270+CodiPop@users.noreply.github.com>
2023-03-12 22:48:48 -0500

    Search field added

--- lib/ui/pages/history-screen.dart
+++ lib/ui/pages/history-screen.dart
@@ -11,48 +11,64 @@ class HistoryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Historial'),
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: myController.items.length,
-          itemBuilder: (context, index) {
-            return Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                width: MediaQuery.of(context).size.width * 0.85,
-                padding: EdgeInsets.all(16),
-                margin: EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "${myController.items[index]} ", // Muestra la fecha y hora del elemento
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    Text(
-                      "${myController.distance[index]} Km recorridos", // Muestra el texto del elemento
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    Text(
-                      "${myController.duration[index]} Minutos", // Muestra el texto del elemento
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      ElevatedButton(
-                        child: Text('Ver Mapa'),
-                        onPressed: () {
-                          // Aquí iría el código para abrir el mapa
-                        },
-                      ),
-                    ]),
-                  ],
-                ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.all(8),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Buscar por Tramo',
+                border: OutlineInputBorder(),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: myController.items.length,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    padding: EdgeInsets.all(16),
+                    margin: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${myController.items[index]} ", // Muestra la fecha y hora del elemento
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                        Text(
+                          "${myController.distance[index]} Km recorridos", // Muestra el texto del elemento
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        Text(
+                          "${myController.duration[index]} Minutos", // Muestra el texto del elemento
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              ElevatedButton(
+                                child: Text('Ver Mapa'),
+                                onPressed: () {
+                                  // Aquí iría el código para abrir el mapa
+                                },
+                              ),
+                            ]),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
