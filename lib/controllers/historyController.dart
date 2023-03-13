c924a68c4d0f77083e746d6ef13705ae7ca8077a
Santiago Guerrero Martinez <47578270+CodiPop@users.noreply.github.com>
2023-03-12 19:53:00 -0500

    Dates updated

--- lib/controllers/historyController.dart
+++ lib/controllers/historyController.dart
@@ -3,15 +3,15 @@ import 'package:get/get.dart';
 class historyController extends GetxController {
   final List<String> items = [
     "1 de marzo 2023",
-    "2 de marzo 2023",
     "3 de marzo 2023",
-    "4 de marzo 2023",
-    "5 de marzo 2023",
-    "6 de marzo 2023",
     "7 de marzo 2023",
-    "8 de marzo 2023",
-    "9 de marzo 2023",
-    "10 de marzo 2023"
+    "10 de marzo 2023",
+    "12 de marzo 2023",
+    "15 de marzo 2023",
+    "17 de marzo 2023",
+    "28 de marzo 2023",
+    "29 de marzo 2023",
+    "30 de marzo 2023"
   ].obs;
   final List<String> distance =
       ["25", "83", "14", "69", "55", "42", "91", "18", "76", "29"].obs;
