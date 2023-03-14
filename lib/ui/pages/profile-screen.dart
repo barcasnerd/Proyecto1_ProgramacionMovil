49757d8ebf12ed0add47031f37bf0caa47450498
Yassg85 <123429815+Yassg85@users.noreply.github.com>
2023-03-13 19:23:36 -0500

    mis commit

--- lib/ui/pages/profile-screen.dart
+++ lib/ui/pages/profile-screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -13,5 +13,105 @@ class ProfileScreen extends StatelessWidget {
         child: Text("ProfileScreen"),
       ),
     );
+  }
+}
+*/
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String _username = 'Augusto';
+  String _bio = 'Hola, Augusto';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profilescreen'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () => _editProfile(context),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+              'https://randomuser.me/api/portraits/men/1.jpg',
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            _username,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            _bio,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _editProfile(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: Text('Edit Profile'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
+                onChanged: (value) => _username = value,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Bio',
+                ),
+                onChanged: (value) => _bio = value,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: Text('Save'),
+              onPressed: () => _saveProfile(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _saveProfile(BuildContext context) {
+    // TODO: Implement logic to save profile changes
+    Navigator.pop(context);
+    setState(() {});
   }
 }
