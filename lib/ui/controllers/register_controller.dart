f7291ba155903a1f865d31bf5fe20c3a25d15026
Santiago Palmet Salcedo <santi1226@gmail.com>
2023-03-14 01:06:28 -0500

    Register page done
    Veo la luz al final del tunel.

---
+++ lib/ui/controllers/register_controller.dart
@@ -0,0 +1,18 @@
+//aquí van cosas que todavia no tengo idea XD
+import 'package:get/get.dart';
+
+class RegisterController extends GetxController {
+  final _username = ''.obs;
+  final _fullName = ''.obs;
+  final _email = ''.obs;
+  late String selectedGender;
+
+  void setGender() {
+    String;
+  }
+
+  //temp route
+  void changePageHome() {
+    Get.toNamed('/home');
+  }
+}
