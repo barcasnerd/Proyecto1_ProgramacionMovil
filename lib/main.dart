20ac35d2eb04749dd6c1c147031aea7ace309967
Jair Barcasnegras <jabares2010@gmail.com>
2023-03-14 00:13:20 -0500

    Revert "13 historial de recorridos"

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
-import 'package:exercise_tracker/ui/controllers/HistoryController.dart';
 import 'package:exercise_tracker/ui/pages/app.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 void main() {
   // TODO: Put the injection of the controller
-
-  Get.lazyPut<HistoryController>(() => HistoryController());
-  // TODO: Add the main app widget page
-
   runApp(App());
 }
