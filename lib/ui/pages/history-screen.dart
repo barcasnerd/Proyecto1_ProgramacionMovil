ad8d3834649246fd88849a3b6d038a398d917594
Santiago Guerrero Martinez <47578270+CodiPop@users.noreply.github.com>
2023-03-14 20:03:57 -0500

    added deletion funcionality

--- lib/ui/pages/history-screen.dart
+++ lib/ui/pages/history-screen.dart
@@ -94,6 +94,21 @@ class HistoryScreen extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: [
                                 ElevatedButton(
+                                  onPressed: () =>
+                                      {_deleteHistory(context, index)},
+                                  child: Text(
+                                    'Eliminar Recorrido',
+                                    style: TextStyle(
+                                        //fontSize: 20,
+                                        ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors
+                                        .red, // Cambia el color del botón a rojo
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                ElevatedButton(
                                   child: Text('Ver Mapa'),
                                   onPressed: () {
                                     myController.changePage();
@@ -111,6 +126,58 @@ class HistoryScreen extends StatelessWidget {
           //FloatingActionButton(onPressed: () => {myController.editarItem()})
         ],
       ),
+    );
+  }
+
+  void _deleteHistory(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                width: MediaQuery.of(context).size.width * 0.28,
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${myController.items[index]} ", // Muestra la fecha y hora del elemento
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Text(
+                      "${myController.distance[index]} Km recorridos", // Muestra el texto del elemento
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    Text(
+                      "${myController.duration[index]} Minutos", // Muestra el texto del elemento
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+              ),
+              Text("¿Esta seguro que desea eliminar este recorrido? "),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: Text('Confirmar'),
+              onPressed: () => {},
+            ),
+          ],
+        );
+      },
     );
   }
 }
